209311f1d23ecf69145b3c0cfbfe360faec0ab54
Balaji Bandgar <balajibandgar26@gmail.com>
2025-09-02 00:01:45 +0530

    GPU section is done and task manager is done for now

--- src/apps/Taskmanager/Taskmanager.jsx
+++ src/apps/Taskmanager/Taskmanager.jsx
@@ -1,5 +1,5 @@
 
-import { Signal } from "lucide-react";
+import { Cpu, Gpu, MemoryStick, Microchip, Signal, Wifi } from "lucide-react";
 import { useEffect, useState } from "react"
 import { Line, LineChart, ResponsiveContainer, YAxis } from "recharts";
 
@@ -9,6 +9,14 @@ export function Taskmanager(){
     const [memoryData, setMemoryData] = useState([])
     const [diskData, setDiskData] = useState([])
     const [wifiData, setWifiData] = useState([])
+    const [gpuData, setGpudata] = useState({
+        gpu3D: [],
+        copy:[],
+        videoDecode:[],
+        videoProcessing:[],
+        shared:[],
+        dedicated:[],
+    })
 
     useEffect(() => {
         const generateInitialData = (baseValue, variance = 10, points = 60) => {
@@ -22,6 +30,14 @@ export function Taskmanager(){
         setMemoryData(generateInitialData(20, 5));
         setDiskData(generateInitialData(30, 17));
         setWifiData(generateInitialData(35, 25))
+        setGpudata({
+            gpu3D:  generateInitialData(8, 12),
+            copy: generateInitialData(2, 3),
+            videoDecode: generateInitialData(5, 8),
+            videoProcessing: generateInitialData(1, 2),
+            shared: generateInitialData(7, 8),
+            dedicated: generateInitialData(10, 6),
+            })
     }, [])
 
     useEffect(() => {
@@ -42,6 +58,14 @@ export function Taskmanager(){
             setMemoryData(prev => updateData(prev, 20, 3));
             setDiskData(prev => updateData(prev, 27, 12));
             setWifiData(prev=> updateData(prev, 35, 15))
+            setGpudata(prev => ({
+                gpu3D:  updateData(prev.gpu3D, 8, 6),
+                copy: updateData(prev.gpu3D, 2, 2),
+                videoDecode: updateData(prev.gpu3D, 5, 4),
+                videoProcessing: updateData(prev.gpu3D, 1, 1),
+                shared: updateData(prev.gpu3D, 9, 6),
+                dedicated: updateData(prev.gpu3D, 12, 4),
+            }))
         }, 1000)
 
         return () => clearInterval(interval)
@@ -69,30 +93,45 @@ export function Taskmanager(){
 
 
     return(
-        <div className="bg-[#191919] h-full text-white overflow-y-auto">
+         <div className="bg-[#191919] h-full text-white overflow-y-auto">
             <div className="border-b border-gray-600 p-2">Performance</div>
             <div className="flex">
                 <div className="w-[15%] p-2">
-                    <div onClick={() => setOption("CPU")} className="p-2 cursor-pointer hover:bg-black/20">
-                        <p className="text-2xl">CPU</p>
-                        <p className="text-sm">27% 3.28GHz</p>
+                    <div onClick={() => setOption("CPU")} className="flex items-center gap-2 cursor-pointer hover:bg-black/20">
+                        <Cpu size={24} className="ml-2 text-red-500"/>
+                        <div className="p-2">
+                            <p className="text-2xl">CPU</p>
+                            <p className="text-xs">{getCurrentValue(cpuData)}% 3.28GHz</p>
+                        </div>
                     </div>
-                    <div onClick={() => setOption("Memory")} className="p-2 cursor-pointer hover:bg-black/20">
-                        <p>Memory</p>
-                        <p className="text-sm">9.0/16 GB (56%)</p>
+                    <div onClick={() => setOption("Memory")} className="flex items-center gap-2 cursor-pointer hover:bg-black/20">
+                        <MemoryStick size={24} className="ml-2 text-purple-500"/>
+                        <div className="p-2">
+                            <p>Memory</p>
+                        <p className="text-xs">8/128 GB {getCurrentValue(memoryData)}%</p>
+                        </div>
                     </div>
-                    <div onClick={() => setOption("Disk")} className="p-2 cursor-pointer hover:bg-black/20">
-                        <p>Disk</p>
-                        <p className="text-sm">SSD 27%</p>
+                    <div onClick={() => setOption("Disk")} className="flex items-center gap-2 cursor-pointer hover:bg-black/20">
+                        <Microchip size={24} className="ml-2 text-yellow-500"/>
+                        <div className="p-2">
+                            <p>Disk</p>
+                            <p className="text-xs">SSD {getCurrentValue(diskData)}%</p>
+                        </div>
                     </div>
-                    <div onClick={() => setOption("Wi-Fi")} className="p-2 cursor-pointer hover:bg-black/20">
-                        <p>Wi-Fi</p>
-                        <p className="text-sm">wi-fi 5.5 Mbps</p>
+                    <div onClick={() => setOption("Wi-Fi")} className="flex items-center gap-2 cursor-pointer hover:bg-black/20">
+                        <Wifi size={24} className="ml-2 text-blue-500"/>
+                        <div className="p-2">
+                            <p>Wi-Fi</p>
+                            <p className="text-xs">wi-fi 5.5 Mbps</p>
+                        </div>
                     </div>
-                    <div onClick={() => setOption("GPU")} className="p-2 cursor-pointer hover:bg-black/20">
-                        <p>GPU</p>
-                        <p className="text-sm">NVIDIA GeForce RTX 2050</p>
-                        <span className="text-sm">0% (39 °C)</span>
+                    <div onClick={() => setOption("GPU")} className="flex items-center gap-2 cursor-pointer hover:bg-black/20">
+                        <Gpu size={24} className="ml-2 text-green-500"/>
+                        <div className="p-2">
+                            <p>GPU</p>
+                            <p className="text-xs">NVIDIA GeForce RTX 7050</p>
+                            <span className="text-xs">{getCurrentValue(gpuData)}% (39 °C)</span>
+                        </div>
                     </div>
                 </div>
 
@@ -249,16 +288,17 @@ export function Taskmanager(){
 
                     {option === "Disk" && (
                     <>
+                    <div>
                     <div className="text-xl flex items-center justify-between">
                         <p>Disk 0 (C: D)</p>
-                        <p>NVMe SOLIDHM SSDPFINX512GLZ</p>
+                        <p>NVMe SOLIDIHM SSDPFINX512GLZ</p>
                     </div>
                     <div className="flex items-center justify-between text-xs text-gray-500 mt-1">
                         <p>Active time</p>
                         <p>{getCurrentValue(diskData)}%</p>
                     </div>
                     <div className="h-60 pt-4 border [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_32px),repeating-linear-gradient(90deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_32px)]">
-                        <GraphComponent data={diskData} color="#ffd60a"/>
+                        <GraphComponent data={diskData} color="#ffd60a" />   
                     </div>
                     <div className="flex items-center justify-between text-xs text-gray-500">
                         <p>60 seconds</p>
@@ -277,23 +317,22 @@ export function Taskmanager(){
                                 </div>
                             </div>
                             <div className="flex gap-5 mt-1">
-                                <div>
+                                <div className="border-l border-green-500 pl-2">
                                     <p className="text-gray-500 text-sm">Read speed</p>
                                     <p>3.8 KB/s</p>
                                 </div>
-                                <div>
+                                <div className="border-l border-green-500 border-dashed pl-2">
                                     <p className="text-gray-500 text-sm">Write speed</p>
                                     <p>210 KB/s</p>
                                 </div>
                             </div>
                         </div>
-
-                        <div className="w-50 flex justify-between mt-1">
-                            <div className="text-sm text-gray-500">
+                        <div className=" w-50 flex justify-between mt-1">
+                            <div className="text-gray-500 text-sm">
                                 <p>Capacity:</p>
                                 <p>Formatted:</p>
-                                <p>System Disk:</p>
-                                <p>page file:</p>
+                                <p>System disk:</p>
+                                <p>Page file:</p>
                                 <p>Type:</p>
                             </div>
                             <div className="text-sm">
@@ -301,9 +340,10 @@ export function Taskmanager(){
                                 <p>512 TB</p>
                                 <p>Yes</p>
                                 <p>Yes</p>
-                                <p>SSD</p>
+                                <p>SSD (RAID)</p>
                             </div>
                         </div>
+                    </div>
                     </div>
                     </>
                     )}
@@ -360,6 +400,112 @@ export function Taskmanager(){
                     </>
                     )}
 
+
+                    {option === "GPU" && (
+                        <>
+                        <div>
+                            <div className="flex justify-between">
+                                <p>GPU</p>
+                                <p>NAVIDIA GeForce RTX 7050</p>
+                            </div>
+                            <div className="grid grid-cols-2 gap-4 mb-2">
+                                <div>
+                                    <div className="flex justify-between">
+                                        <p className="text-xs text-gray-500">3D</p>
+                                        <p className="text-xs text-gray-500">{getCurrentValue(gpuData.gpu3D)}%</p>
+                                    </div>
+                                    <div className="h-28 pt-4 border [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_26px),repeating-linear-gradient(90deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_28px)]">
+                                        <GraphComponent data={gpuData.gpu3D} color="#ff6b35" height="100%" />
+                                    
+                                    </div>
+                                </div>
+                                
+                                <div>
+                                    <div className="flex justify-between">
+                                        <p className="text-xs text-gray-500">Copy</p>
+                                    <p className="text-xs text-gray-500">{getCurrentValue(gpuData.copy)}%</p>
+                                    </div>
+                                    <div className="h-28 pt-4 border [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_26px),repeating-linear-gradient(90deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_28px)]">
+                                        <GraphComponent data={gpuData.copy} color="#00f5ff" height="100%" />
+                                </div>
+                                </div>
+                                
+                                <div>
+                                    <div className="flex justify-between">
+                                        <p className="text-xs text-gray-500">Video Decode</p>
+                                    <p className="text-xs text-gray-500">{getCurrentValue(gpuData.videoDecode)}%</p>
+                                    </div>
+                                    <div className="h-28 pt-4 border [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_26px),repeating-linear-gradient(90deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_28px)]">
+                                        <GraphComponent data={gpuData.videoDecode} color="#7209b7" height="100%" />
+                                </div>
+                                </div>
+                                
+                                <div>
+                                    <div className="flex justify-between">
+                                        <p className="text-xs text-gray-500">Video Processing</p>
+                                    <p className="text-xs text-gray-500">{getCurrentValue(gpuData.videoProcessing)}%</p>
+                                    </div>
+                                    <div className="h-28 pt-4 border [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_26px),repeating-linear-gradient(90deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_28px)]">
+                                        <GraphComponent data={gpuData.videoProcessing} color="#ff1744" height="100%" />
+                                </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex justify-between">
+                                    <p className="text-xs text-gray-500">Shared GPU memory usage</p>
+                                <p className="text-xs text-gray-500">{getCurrentValue(gpuData.shared)}%</p>
+                                </div>
+                                <div className="h-10 pt-2 border [background-image:repeating-linear-gradient(0deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_12px),repeating-linear-gradient(90deg,rgba(255,255,255,0.05)_0px,rgba(255,255,255,0.05)_1px,transparent_1px,transparent_14px)]">
+                                    <GraphComponent data={gpuData.shared} color="#00c851" height="100%" />
+                            </div>
+                            </div>
+
+                            <div className="flex gap-5 mt-4">
+                                <div className="text-lg">
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Utilization</p>
+                                        <p>10%</p>
+                                    </div>
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Gpu Memory</p>
+                                        <p>3.9/7.9 GB</p>
+                                    </div>
+                                </div>
+                                <div className="text-sm">
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Dedicated GPU memory</p>
+                                        <p>0.0/4.0 GB</p>
+                                    </div>
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Shared GPU memory</p>
+                                        <p>0.0/3.9 GB</p>
+                                    </div>
+                                    <div>
+                                        <p className="text-gray-500 text-sm">Temperature</p>
+                                        <p>39 °C</p>
+                                    </div>
+                                </div>
+                                <div className="flex gap-4 mt-1">
+                            <div className="text-gray-500 text-sm">
+                                <p>Driver version:</p>
+                                <p>Driver date:</p>
+                                <p>DirectX version:</p>
+                                <p>Physical location:</p>
+                                <p>Hardware reserved memory:</p>
+                            </div>
+                            <div className="text-sm">
+                                <p>32.0.43.7258</p>
+                                <p>25-04-2025</p>
+                                <p>15 (Fl 15.1)</p>
+                                <p>PCI bus 1, device 0, function 0</p>
+                                <p>131 MB</p>
+                            </div>
+                        </div>
+                            </div>
+                        </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
